7fe7f948d932278f8eff62398033199ea6be1dd0
Brill-Prime <brillprimeltd@gmail.com>
2025-12-18 22:29:53 -0800

    Fix ESLint dependency conflict: upgrade to ESLint 9 and update config for Next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextPlugin from '@next/eslint-plugin-next'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+/** @type {import("eslint").Linter.FlatConfig[]} */
+const config = [
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+]
 
-export default eslintConfig;
+export default config;
